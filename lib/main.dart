82c75382bb9d38c8bd8407bad1a9860a672650cf
Vaxart69 <158302778+Vaxart69@users.noreply.github.com>
2025-06-04 13:09:09 +0800

    added animations and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-<<<<<<< Updated upstream
-import 'provider.dart';
-import 'calculatorform.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ComponentProvider()..listenToComponents(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Grade Calculator',
-        theme: ThemeData(
-          textTheme: GoogleFonts.poppinsTextTheme(),
-        ),
-        home: Calculator(),
-=======
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -31,28 +12,24 @@ class GradeCalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.light,
-        systemNavigationBarColor: Color(0xFF121212), 
+        systemNavigationBarColor: Color(0xFF121212),
         systemNavigationBarIconBrightness: Brightness.light,
         systemNavigationBarDividerColor: Colors.transparent,
       ),
     );
-    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Grade Calculator',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFF121212),
-        
-        textTheme: GoogleFonts.poppinsTextTheme(
-          
-        )
->>>>>>> Stashed changes
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-    ),
-  );
+      // TODO: Set your home: property here, e.g. home: StartingPage(),
+    );
+  }
 }
